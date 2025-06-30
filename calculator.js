c445e62fbc427835d5777a6bc81c90bdc03b1a5c
RAHUL JADAV <rj.jadav22@gmail.com>
2025-06-30 10:41:19 +0530

    Added new line as delimiter

--- calculator.js
+++ calculator.js
@@ -4,7 +4,9 @@ function add(numbers) {
     //Return 0 if empty string passed
     if (!numbers) return 0;
 
-    return numbers.split(",").map(number => parseInt(number)).reduce((sum, current) => sum + current, 0);
+    return numbers.split(/[\n,]/)
+    .map(number => parseInt(number))
+    .reduce((sum, current) => sum + current, 0);
 }
 
 module.exports = {
--- calculator.test.js
+++ calculator.test.js
@@ -14,4 +14,8 @@ describe('Create string calculator', () => {
         expect(add("1,5")).toBe(6);
         expect(add("3,5")).toBe(8);
     })
+
+    test('return sum if new line is used as delimiter', () => {
+        expect(add('1\n2,3')).toBe(6);
+    })
 })
