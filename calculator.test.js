cdfc9f394032f06c90d006e5cbe32f2cfde6958a
RAHUL JADAV <rj.jadav22@gmail.com>
2025-06-30 11:58:51 +0530

    Added readme file and minor refactoring

--- calculator.test.js
+++ calculator.test.js
@@ -5,25 +5,25 @@ describe('Create string calculator', () => {
         expect(add('')).toBe(0)
     })
 
-    test('returns the number itself for a single number', () => {
+    test('Return the number itself for a single number', () => {
         expect(add("1")).toBe(1);
         expect(add("5")).toBe(5);
     });
 
-    test('return sum for two comma separated value as string', () => {
+    test('Return sum for two comma separated value as string', () => {
         expect(add("1,5")).toBe(6);
         expect(add("3,5")).toBe(8);
     });
 
-    test('return sum if new line is used as delimiter', () => {
+    test('Return sum if new line is used as delimiter', () => {
         expect(add('1\n2,3')).toBe(6);
     });
 
-    test('return sum if other special charaters are used', () => {
+    test('Return sum if other special charaters are used in before new line', () => {
         expect(add("//;\n1;2")).toBe(3);
     })
 
-    test('throws an error if negative numbers are present', () => {
+    test('Throws an error if negative numbers are present', () => {
         expect(() => add("1,-2,3,-5")).toThrow("Negative numbers are not allowed: -2,-5");
     });
 })
---
+++ readme.md
@@ -0,0 +1,28 @@
+## Incubyte Assignment 
+
+To setup do:
+```bash
+npm install
+# or
+yarn install
+```
+
+To run the test cases:
+
+```bash
+npm run test
+# or
+yarn test
+```
+
+## Handled following scenarios in TDD
+- Return 0 if empty string or null is passed
+- Return the number itself if single number is passed
+- Retrun the sum of two or more comma separated values
+- Return sum if new line character is used as delimiter
+- Support if custom delimiters
+- Handled negative numbers' scenario
+
+## Scope of Imporvements
+- We can limit the max number for ex: Ignore the number more than 1000
+- Throw error if Alphabets are used
