366acc6042526acf4c34b91e0f54b5850327cc47
RAHUL JADAV <rj.jadav22@gmail.com>
2025-06-30 10:36:57 +0530

    Added functionality and test cases to handle comma separated values without special characters

--- calculator.js
+++ calculator.js
@@ -1,8 +1,10 @@
 
 function add(numbers) {
-    if(!numbers) return 0;
 
-    return null;
+    //Return 0 if empty string passed
+    if (!numbers) return 0;
+
+    return numbers.split(",").map(number => parseInt(number)).reduce((sum, current) => sum + current, 0);
 }
 
 module.exports = {
--- calculator.test.js
+++ calculator.test.js
@@ -4,4 +4,14 @@ describe('Create string calculator', () => {
     test('Return 0 if blank string is passed', () => {
         expect(add('')).toBe(0)
     })
+
+    test('returns the number itself for a single number', () => {
+        expect(add("1")).toBe(1);
+        expect(add("5")).toBe(5);
+    });
+
+    test('return sum for two comma separated value as string', () => {
+        expect(add("1,5")).toBe(6);
+        expect(add("3,5")).toBe(8);
+    })
 })
